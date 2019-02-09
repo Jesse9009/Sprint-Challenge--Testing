f414f3cd8c015fa3774f267d04a43f2532d00884
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-09 12:00:17 -0600

    tests written for GET and POST requests for the '/games' endpoint

--- server.test.js
+++ server.test.js
@@ -2,3 +2,55 @@ const request = require('supertest');
 
 const server = require('./server');
 const db = require('./db');
+
+describe('the route handlers', () => {
+  describe('GET /games', () => {
+    test('responds with a 200 status', async () => {
+      const response = await request(server).get('/games');
+      expect(response.status).toBe(200);
+    });
+    test('responds with json', async () => {
+      const response = await request(server).get('/games');
+      expect(response.type).toMatch(/json/i);
+    });
+    test('always responds with an array, even if there are no games', async () => {
+      const response = await request(server).get('/games');
+      const rows = await expect(response.body).toEqual([]);
+    });
+  });
+  describe('POST /games', () => {
+    test('responds with a 201 status', async () => {
+      const body = {
+        title: 'Red Dead Redemption 2',
+        genre: 'Open World Action-adventure',
+        releaseYear: 2018
+      };
+      const response = await request(server)
+        .post('/games')
+        .send(body);
+      expect(response.status).toBe(201);
+    });
+    test('responds with a 422 status if request is missing data', async () => {
+      const body = {
+        title: 'Red Dead Redemption 2',
+        genre: '',
+        releaseYear: 2018
+      };
+      const response = await request(server)
+        .post('/games')
+        .send(body);
+      expect(response.status).toBe(422);
+    });
+    test('responds with a 409 status if a duplicate game is entered', async () => {
+      const body = {
+        title: 'Pacman',
+        genre: 'Arcade',
+        releaseYear: 1980
+      };
+      const response = await request(server)
+        .post('/games')
+        .send(body);
+      expect(response.status).toBe(409);
+    });
+  });
+});
